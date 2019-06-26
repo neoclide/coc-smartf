dd94f03f3097669dcde1ae9dd6c8fcb2f9c01750
chemzqm <chemzqm@gmail.com>
2019-06-26 15:54:02 +0800

    fix positions for none word character

--- src/util.ts
+++ src/util.ts
@@ -6,12 +6,13 @@ interface Position {
 export function getPositions(character: string, lines: string[], matchWord: boolean, offset = 0): Position[] {
   let positions: Position[] = []
   let i = 0
+  let isWordChar = wordChar(character)
   for (let line of lines) {
     let start = i == 0 ? offset : 0
     for (let j = start; j < line.length; j++) {
       if (line[j] == character) {
-        if (!matchWord || (j == 0 || !wordChar(line[j - 1]))) {
-          positions.push({line: i, character: j})
+        if (!matchWord || !isWordChar || (j == 0 || !wordChar(line[j - 1]))) {
+          positions.push({ line: i, character: j })
         }
       }
     }
